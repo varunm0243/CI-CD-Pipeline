cbc29ecef6752d5f5db411ea936408cf2befd748
varunm0243 <110885630+varunm0243@users.noreply.github.com>
2022-09-24 09:22:36 +0530

    Add files via upload

---
+++ prod-phase-back-end/sapientpowersports/src/main/java/com/sapientpowersports/sapientpowersports/SapientpowersportsApplication.java
@@ -0,0 +1,15 @@
+package com.sapientpowersports.sapientpowersports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapientpowersportsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SapientpowersportsApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ prod-phase-back-end/sapientpowersports/src/test/java/com/sapientpowersports/sapientpowersports/SapientpowersportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapientpowersports.sapientpowersports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapientpowersportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
